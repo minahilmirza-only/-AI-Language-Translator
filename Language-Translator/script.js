f5da7deebae8f1b2c8b59d9b794dd038712a3f08
minahilmirza-only <minahilmirza9876@gmail.com>
2026-09-04 18:01:49 +0500

    clearbtn,clear btn js

--- Language-Translator/script.js
+++ Language-Translator/script.js
@@ -1,3 +1,7 @@
 const translateBtn = document.getElementById('translateBtn');
 const inputText = document.getElementById('inputText');
 const targetlangs = document.getElementById('targetlangs');
+const outputText = document.getElementById('outputText');
+const clearBtn = document.getElementById('clearBtn');
+const copyBtn = document.getElementById('copyBtn');
+const speakBtn = document.getElementById('speakBtn');
