5e6b64c111bdb3ec2a92e81126c73723a291294f
minahilmirza-only <minahilmirza9876@gmail.com>
2026-09-04 18:05:29 +0500

    event listener for translation

--- Language-Translator/script.js
+++ Language-Translator/script.js
@@ -1,7 +1,18 @@
-const translateBtn = document.getElementById('translateBtn');
+const TranslateBtn = document.getElementById('Translatebtn');
 const inputText = document.getElementById('inputText');
 const targetlangs = document.getElementById('targetlangs');
 const outputText = document.getElementById('outputText');
 const clearBtn = document.getElementById('clearBtn');
 const copyBtn = document.getElementById('copyBtn');
 const speakBtn = document.getElementById('speakBtn');
+
+TranslateBtn.addEventListener('click', async () => {
+  const text = inputText.value.trim();
+  const targetLang = targetlangs.value();
+
+  if (text === '') {
+    alert('Please enter text to translate.');
+    return;
+  }
+
+});
